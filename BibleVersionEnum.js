233e9c5a262ba03d3b64478e64494bb259cd309c
Lucas Tonussi <lptonussi@gmail.com>
2022-07-21 23:14:06 -0300

    Rocket it

---
+++ BibleVersionEnum.js
@@ -0,0 +1,14 @@
+const BibleVersionEnum = {
+  EMTV: "EMTV",
+  BYZ: "BYZ",
+  WPNT: "WPNT",
+  ACF: "ACF",
+  ITARIVE: "ITA",
+  FREMRTN: "FRE",
+  ISV: "ISV",
+  LTT: "LTT"
+}
+
+Object.freeze(BibleVersionEnum);
+
+module.exports.BibleVersionEnum = BibleVersionEnum;
